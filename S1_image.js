b0b2e8f0a14568d1187a67120217871ba9cef3d9
AndyBraun <AndyBraun@posteo.de>
2021-08-07 22:00:36 +0200

    Update S1_image.js

--- S1_image.js
+++ S1_image.js
@@ -9,14 +9,3 @@ function setup() {
 function evaluatePixel(sample) {
   return [sample.VV*1.5 ];
 }
-
-
-//Limit search to one date
-function preProcessScenes (collections) {
-    var allowedDates = ["2021-07-14"]; //Allowed dates
-    collections.scenes.orbits = collections.scenes.orbits.filter(function (orbit) {
-        var orbitDateFrom = orbit.dateFrom.split("T")[0];
-        return allowedDates.includes(orbitDateFrom);
-    })
-    return collections
-}
